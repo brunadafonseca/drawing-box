b716e82693be2dafb18d752993ca402b900e6861
brunadafonseca <brunadafonseca@gmail.com>
2018-04-10 00:04:42 +0200

    Add display grid

--- app.js
+++ app.js
@@ -59,6 +59,29 @@ function draw(e) {
   }
 }
 
+
+function drawMobile(e) {
+  if (!isDrawing) return
+  
+  ctx.beginPath()
+  ctx.moveTo(lastX, lastY)
+  ctx.lineTo(e.touches[0].pageX, e.touches[0].pageY)
+  ctx.stroke()
+  
+  lastX = e.touches[0].pageX
+  lastY = e.touches[0].pageY
+  
+  if (rainbow) {
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+
+    hue++
+
+    if (hue >= 360) {
+      hue = 0
+    }
+  }
+}
+
 function saveDrawing() {
   const blank = document.createElement('canvas');
   blank.width = canvas.width;
@@ -66,7 +89,8 @@ function saveDrawing() {
 
   if (canvas.toDataURL() == blank.toDataURL()) return
 
-  const savedDrawings = document.querySelector('.drawings')
+  const drawings = document.querySelector('.drawings')
+  const drawingsWrapper = document.querySelector('.saved-drawings')
   const noDrawings = document.querySelector('.no-drawings')
   
   canvas.toBlob(blob => {
@@ -80,18 +104,25 @@ function saveDrawing() {
     newImg.src = url
 
     const drawingFrame = document.createElement('div')
-    drawingFrame.setAttribute('class', 'drawing-frame')
+
+    drawingFrame.setAttribute('class', 'drawing-frame fadein')
 
     drawingFrame.appendChild(newImg)
     const downloadButton = createDownloadButton(blob)
     const div = document.createElement('div')
-    div.setAttribute('class', 'drawing')
+    div.setAttribute('class', 'drawing fadein')
 
     div.appendChild(drawingFrame)
     div.appendChild(downloadButton)
 
-    noDrawings.classList.add('hidden')
-    savedDrawings.appendChild(div)
+    // drawings.appendChild(div)
+    const test = document.querySelectorAll('.drawing');
+    for (var i = 0; i < test.length; i++) {
+      test[i].classList.add('fadeout')
+      test[i].classList.remove('fadein')
+  }
+    drawings.insertBefore(div, drawings.firstChild);
+    drawingsWrapper.classList.remove('hidden')
   })
 }
 
@@ -119,3 +150,18 @@ canvas.addEventListener('mousedown', (e) => {
 })
 canvas.addEventListener('mouseup', () => isDrawing = false)
 canvas.addEventListener('mouseout', () => isDrawing = false)
+canvas.addEventListener('touchmove', drawMobile)
+canvas.addEventListener('touchstart', (e) => {
+  isDrawing = true
+  lastX = e.touches[0].pageX
+  lastY = e.touches[0].pageY
+})
+canvas.addEventListener('touchend', () => isDrawing = false)
+canvas.addEventListener('touchcancel', () => isDrawing = false)
+
+// var tuio =
+// { cursors:[] ,_data:{} , _touchstart: function(touch){this._create_event('touchstart',touch,{});},_touchmove:function(touch){this._create_event('touchmove',touch,{});},_touchend:function(touch){this._create_event('touchend',touch,{});},_create_event:function(name,touch,attrs){var evt=document.createEvent('CustomEvent');evt.initEvent(name,true,true);evt.touches=this.cursors;evt.targetTouches=this._get_target_touches(touch.target);evt.changedTouches=[touch];for(var attr in attrs){if(attrs.hasOwnProperty(attr)){evt[attr]=attrs[attr];}}
+// if(touch.target){touch.target.dispatchEvent(evt);}else{document.dispatchEvent(evt);}},_get_target_touches:function(element){var targetTouches=[];for(var i=0;i<this.cursors.length;i++){var touch=this.cursors[i];if(touch.target==element){targetTouches.push(touch);}}
+// return targetTouches;},callback:function(type,sid,fid,x,y,angle){var data;if(type!==3){data=this._data[sid];}else{data={sid:sid,fid:fid};this._data[sid]=data;}
+// data.identifier=sid;data.pageX=window.innerWidth*x;data.pageY=window.innerHeight*y;data.target=document.elementFromPoint(data.pageX,data.pageY);switch(type){case 3:this.cursors.push(data);this._touchstart(data);break;case 4:this._touchmove(data);break;case 5:this.cursors.splice(this.cursors.indexOf(data),1);this._touchend(data);break;default:break;}
+// if(type===5){delete this._data[sid];}}};function tuio_callback(type,sid,fid,x,y,angle){tuio.callback(type,sid,fid,x,y,angle);}
